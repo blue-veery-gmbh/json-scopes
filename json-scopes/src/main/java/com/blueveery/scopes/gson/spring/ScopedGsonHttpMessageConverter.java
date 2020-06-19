e2c8eaad7047b87d041f92bb90fb73027ad1b903
Albert Piekielny <albert.piekielny@blue-veery.com>
2020-06-19 09:45:21 +0200

    - Correct throwing exception type

--- json-scopes/src/main/java/com/blueveery/scopes/gson/spring/ScopedGsonHttpMessageConverter.java
+++ json-scopes/src/main/java/com/blueveery/scopes/gson/spring/ScopedGsonHttpMessageConverter.java
@@ -1,6 +1,7 @@
 package com.blueveery.scopes.gson.spring;
 
 import com.blueveery.core.model.BaseEntity;
+import com.google.gson.JsonIOException;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.lang.Nullable;
 
@@ -12,7 +13,7 @@ import java.lang.reflect.Type;
 public class ScopedGsonHttpMessageConverter extends GsonHttpMessageConverter {
 
     @Override
-    protected void writeInternal(Object o, @Nullable Type type, Writer writer) throws Exception {
+    protected void writeInternal(Object o, @Nullable Type type, Writer writer) throws JsonIOException {
         getGson().getAdapter(BaseEntity.class);
         if (type instanceof ParameterizedType) {
             getGson().toJson(o, type, writer);
